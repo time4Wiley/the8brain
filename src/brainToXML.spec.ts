af6f94d8701df0c84f644763709be91e100a3e4c
Wei Sun <sunwei415@126.com>
2022-02-08 05:19:15 +0800

    generating brain xml skeleton

--- src/brainToXML.spec.ts
+++ src/brainToXML.spec.ts
@@ -3,10 +3,28 @@ import { create } from 'xmlbuilder2';
 
 test('brain2xml', (t:ExecutionContext) => {
 
-  const root = create({version: '1.0'});
+  const root = create({version: '1.0', encoding:'UTF-8'}).ele({});
 
   const xml = root.end({ prettyPrint: true });
 
   console.log(xml)
   t.true(true)
+})
+
+test('doc type', (t:ExecutionContext) => {
+
+  const root = create().dtd({}).doc()
+
+  root.ele("BrainData")
+    .ele("Source")
+    .up()
+    .ele("Attributes")
+    .up()
+    .ele("Thoughts")
+    .up()
+    .ele("Links").up().ele('Entries').up().ele('Attachments').up()
+
+  console.log(root.end({ prettyPrint: true }))
+  t.true(true)
+
 })
