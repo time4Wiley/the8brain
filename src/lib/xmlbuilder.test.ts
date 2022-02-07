b3f0349b81edd4508ec9cd1520c8f603ec43002e
Wei Sun <sunwei415@126.com>
2022-02-08 03:08:03 +0800

    installed xmlbuilder and test on it

---
+++ src/lib/xmlbuilder.test.ts
@@ -0,0 +1,14 @@
+import { create } from 'xmlbuilder2';
+
+
+const root = create({ version: '1.0' })
+  .ele('root', { att: 'val' })
+  .ele('foo')
+  .ele('bar').txt('foobar').up()
+  .up()
+  .ele('baz').up()
+  .up();
+
+// convert the XML tree to string
+const xml = root.end({ prettyPrint: true });
+console.log(xml);
