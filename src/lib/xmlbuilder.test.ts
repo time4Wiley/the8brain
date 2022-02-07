517e5e999c0df14ea9cfa3b8bb2c7b81ffa3949a
Wei Sun <sunwei415@126.com>
2022-02-08 03:13:00 +0800

    xmlbuilder test passed

--- src/lib/xmlbuilder.test.ts
+++ src/lib/xmlbuilder.test.ts
@@ -1,14 +1,21 @@
+import test, {ExecutionContext } from 'ava';
 import { create } from 'xmlbuilder2';
 
+test('xml generating', (t:ExecutionContext) => {
 
-const root = create({ version: '1.0' })
-  .ele('root', { att: 'val' })
-  .ele('foo')
-  .ele('bar').txt('foobar').up()
-  .up()
-  .ele('baz').up()
-  .up();
+  const root = create({ version: '1.0' })
+    .ele('root', { att: 'val' })
+    .ele('foo')
+    .ele('bar').txt('foobar').up()
+    .up()
+    .ele('baz').up()
+    .up();
 
 // convert the XML tree to string
-const xml = root.end({ prettyPrint: true });
-console.log(xml);
+  const xml = root.end({ prettyPrint: true });
+  console.log(xml);
+
+  t.true(xml.length > 0)
+})
+
+
